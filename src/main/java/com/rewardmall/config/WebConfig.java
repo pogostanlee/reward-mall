90d6970d8861b120d006a320fa1628493e05e03c
lee <175937217@qq.com>
2023-12-26 22:49:15 +0800

    全部功能完善

--- src/main/java/com/rewardmall/config/WebConfig.java
+++ src/main/java/com/rewardmall/config/WebConfig.java
@@ -9,6 +9,7 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 //配置拦截器
@@ -20,8 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("/user/login");
+//        strings.add("/admin/exportCustomer");
         //登录接口和注册接口不拦截
-        registry.addInterceptor(loginInterceptor).excludePathPatterns("/user/login");
+        registry.addInterceptor(loginInterceptor).excludePathPatterns(strings);
+
     }
     //配置日期格式化
     @Bean
--- src/main/java/com/rewardmall/service/impl/AdminServiceImpl.java
+++ src/main/java/com/rewardmall/service/impl/AdminServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.net.URLEncoder;
 import java.util.Date;
 import java.util.List;
 
@@ -71,11 +72,12 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public void exportCustomer(HttpServletResponse response) {
         try {
-            //设置excel下载响应头
-            response.setContentType("application/vnd.ms-excel");
-            response.setCharacterEncoding("utf-8");
-            response.setHeader("Content-disposition", "attachment;filename=customer.xlsx");
 
+            response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            response.setCharacterEncoding("utf-8");
+            // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+            String fileName = URLEncoder.encode("测试", "UTF-8").replaceAll("\\+", "%20");
+            response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
             //获取所有用户信息
             List<Customer> customers = customerMapper.selectAll();
             //创建excel
